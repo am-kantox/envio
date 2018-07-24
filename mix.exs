f48a15158cf2458044c4a815a4f5fb3b0c174907
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2018-07-24 18:22:16 +0200

    Dependencies.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Envio.MixProject do
 
   @app :envio
   @app_name "Envío"
-  @version "0.2.0"
+  @version "0.2.1"
 
   def project do
     [
@@ -30,8 +30,8 @@ defmodule Envio.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 0.8", only: :dev},
-      {:ex_doc, "~> 0.11", only: :dev}
+      {:credo, "~> 0.9", only: :dev},
+      {:ex_doc, "~> 0.18", only: :dev, override: true}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
