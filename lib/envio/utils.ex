269624b20ca46c7e6e74b3cc91ef92a5fe1a8788
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2018-07-27 07:21:25 +0200

    Tests for utilities.

--- lib/envio/utils.ex
+++ lib/envio/utils.ex
@@ -1,16 +1,34 @@
 defmodule Envio.Utils do
-  @moduledoc false
+  @moduledoc """
+  Multipurpose utils, required for the _Envío_ proper functioning, like:
+
+  * `fq_name` to construct fully-qualified names for channels to prevent clashes
+  """
 
   @fq_joiner "."
 
-  @spec fq_name(binary() | {atom(), atom() | binary()}) :: binary()
-  def fq_name(fq_name) when is_binary(fq_name), do: fq_name
-  def fq_name({namespace, channel}), do: fq_name(namespace, channel)
+  @doc """
+  Produces a fully-qualified name out of namespace in a form
+    of a module name atom and a method name (atom or binary.)
 
-  def fq_name(namespace, channel) when is_binary(namespace) and is_binary(channel),
-    do: Enum.join([namespace, channel], @fq_joiner)
+  ## Examples
 
-  @spec fq_name(atom() | binary(), atom() | binary()) :: binary()
+      iex> Envio.Utils.fq_name("foo")
+      "foo"
+      iex> Envio.Utils.fq_name({Foo.Bar, "baz"})
+      "foo/bar.baz"
+      iex> Envio.Utils.fq_name(Foo.Bar, "baz")
+      "foo/bar.baz"
+      iex> Envio.Utils.fq_name(Foo.Bar, :baz)
+      "foo/bar.baz"
+      iex> Envio.Utils.fq_name("Foo.Bar", "baz")
+      "Foo.Bar.baz"
+  """
+  @spec fq_name(binary() | atom() | {atom(), atom() | binary()}, nil | atom() | binary()) :: binary()
+  def fq_name(namespace_or_name, name_or_nil \\ nil)
+  def fq_name(fq_name, nil) when is_binary(fq_name), do: fq_name
+  def fq_name({namespace, channel}, nil), do: fq_name(namespace, channel)
+
   def fq_name(namespace, channel) when is_binary(namespace) and is_binary(channel),
     do: Enum.join([namespace, channel], @fq_joiner)
 
---
+++ test/utils_test.exs
@@ -0,0 +1,4 @@
+defmodule Envio.Utils.Test do
+  use ExUnit.Case, async: true
+  doctest Envio.Utils
+end
