ff2986590ddae821334ff11f85964fb46089d92f
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2018-07-30 06:29:04 +0200

    Use Iteraptor to flatten input for slack.

--- config/test.exs
+++ config/test.exs
@@ -4,10 +4,10 @@ config :envio, :binary_value, "FOO"
 config :envio, :env_value, {:system, "FOO"}
 
 config :envio, :backends, %{
-  Envio.Slack => %{
-    {Spitter, :main} => [
-      hook_url: {:system, "SLACK_ENVIO_HOOK_URL"}
-    ]
-  },
+  # Envio.Slack => %{
+  #   {Spitter, :main} => [
+  #     hook_url: {:system, "SLACK_ENVIO_HOOK_URL"}
+  #   ]
+  # },
   Envio.IOBackend => %{{Spitter, :backends} => []}
 }
