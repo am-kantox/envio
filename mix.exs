116b9b84b12f5b98360270104f71bc9e8da9425f
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2020-03-15 07:22:24 +0100

    Fix formatting in mix.exs

--- mix.exs
+++ mix.exs
@@ -110,7 +110,7 @@ defmodule Envio.MixProject do
   end
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-#  defp elixirc_paths(:dev), do: ["lib", "test/support"]
+  #  defp elixirc_paths(:dev), do: ["lib", "test/support"]
   defp elixirc_paths(:ci), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 end
